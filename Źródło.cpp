a2fdbd1837f316b756981018fd240e00dcb260ad
Spejn <61868442+Spejn@users.noreply.github.com>
2020-03-12 14:56:01 +0100

    Add files via upload

--- Źródło.cpp
+++ Źródło.cpp
@@ -1,35 +1,34 @@
 //Z³ota proporcja 2.1
 
 #include <iostream>
-#include <conio.h>
 using namespace std;
 
 const float fi = 1.618;
 double nLiczbaAB;
 
-void main()
+int main()
 {
 	cout << "Obliczanie zlotej proporcji" << endl;
 	cout << "wpisz 0 aby zakonczyc program" << endl << endl;
 	cout << "Podaj liczbe: ";
 	cin >> nLiczbaAB;
+
 	while (nLiczbaAB !=0)
 	{
-	
-		double nWynik = nLiczbaAB / fi;
+		
 
+		double long nWynik = nLiczbaAB / fi;
 
-		double nLiczbaA = nWynik;
+		double long nLiczbaA = nWynik;
 		(nLiczbaAB != 0);
 		
-		double nLiczbaB = nLiczbaAB - nLiczbaA;
+		double long nLiczbaB = nLiczbaAB - nLiczbaA;
 		cout << "a" << "=" << nLiczbaA << "  b" << "=" << nLiczbaB << endl << endl;
 		cout << "Podaj liczbe: ";
 		cin >> nLiczbaAB;
 	} 
 	cout << "nacisnij dowolny przycisk";
 
-	_getch();
-
+	return 0;
 
 }
